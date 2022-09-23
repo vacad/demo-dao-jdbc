df55d14a8c5caa117dab7d15e9720e3a3fe65d13
Yago Neves <yagolife1@gmail.com>
2022-09-23 14:36:33 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Date birth = new Date();
+		Department dep = new Department(1, "Books");
+		Seller seller = new Seller(21, "Yago", "yagolife1@gmail.com", birth, 10000.0, dep); 
+		
+		System.out.println(dep);
+		System.out.println(seller);
 	}
 
 }
