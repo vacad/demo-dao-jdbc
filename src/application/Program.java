f0b73d97df94c25dd70bcd9fefc92a94d64f1295
Yago Neves <yagolife1@gmail.com>
2022-09-23 14:48:33 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		Date birth = new Date();
 		Department dep = new Department(1, "Books");
 		Seller seller = new Seller(21, "Yago", "yagolife1@gmail.com", birth, 10000.0, dep); 
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(dep);
 		System.out.println(seller);
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
+	
 	void insert(Seller obj);
 
 	void update(Seller obj);
